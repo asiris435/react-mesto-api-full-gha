febdff3b5c65b688bcd7634e80f82731363c99b7
Aleksandr Kazantsev <asiris435@yandex.ru>
2023-08-27 08:38:42 +0300

    feat: добавить crash-test

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -6,6 +6,12 @@ const userRouter = require('./users');
 const cardRouter = require('./cards');
 const NotFoundError = require('../errors/NotFoundError');
 
+router.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 router.use('/signup', signupRouter);
 router.use('/signin', signinRouter);
 router.use(auth);
